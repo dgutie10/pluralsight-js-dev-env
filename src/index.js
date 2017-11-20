4e6a73c88dcbc007b9d2e2c7d652580a986d6252
Diego Gutierrez <Diego.Gutierrez@citrix.com>
2017-11-20 00:19:52 -0500

    module 11  completed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,35 @@
-import numeral from 'numeral';
+import {deleteUser, getUsers} from "./api/userApi";
 import './index.css'
 
-const courseValue = numeral(1000).format('$0,0.00');
 
-console.log(`I would like to pay ${courseValue} for this course!!!!!`); //eslint-disable-line no-console
+
+//Populate Table of users via API call
+getUsers().then(result => {
+    let userBody ="";
+
+    result.forEach(user => {
+        userBody += `<tr>
+        <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a> </td>
+        <td> ${user.id}</td>
+        <td> ${user.firstName}</td>
+        <td> ${user.lastName}</td>
+        <td> ${user.email}</td>
+        </tr>`
+
+        console.log(user); //eslint-disable-line no-console
+    });
+
+    global.document.getElementById('users').innerHTML = userBody;
+
+    const  deleteLink = global.document.getElementsByClassName('deleteUser');
+
+    Array.from(deleteLink, link => {
+        link.onclick = function (event) {
+            const element = event.target;
+            event.preventDefault();
+            deleteUser(element.attributes['data-id'].value);
+            const row = element.parentNode.parentNode;
+            row.parentNode.removeChild(row);
+        }
+    })
+})
